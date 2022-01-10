7ec277adaf9d61b7d931bc3c8dcf8e9a19061b14
joy langat <joy.chemutai@student.moringaschool.com>
2022-01-10 12:38:49 +0300

    add toggle effect and hover effects

--- Js/script.js
+++ Js/script.js
@@ -1,56 +1,90 @@
-$(document).ready(function(){
-    $(".image1").click(function() {
-       $(".image1").hide(1200);
-       $(".p1").toggle(1200);
-       $(".p1").show(1200); 
-    })
-    $("p1").click(function(){
-        $(".p1").hide(1200);
-        $(".image1").toggle(1200);
-        $(".image1").show(1200);
+//UserInterface Logic
+$(document).ready(function() {
+    $("div#design").click(function() {
+      $("div#content").toggle();
+      $("div#design").toggle();
     });
- });
- $(document).ready(function(){
-     $(".image2").click(function(){
-         $(".image2").hide(1200);
-         $(".p2").toggle(1200);
-         $(".p2").show(1200);
-     })
-     $(".p2").click(function(){
-       $(".p2").hide(1200);
-       $(".image2").toggle(1200);
-       $(".image2").show(1200);
-     });
- });
- $(document).ready(function(){
-     $(".image3").click(function(){
-        $(".image3").hide(1200);
-        $(".p3").toogle(1200);
-        $(".p3").show(1200);
-     })
-     $(".p3").click(function(){
-        $(".p3").hide(1200);
-        $(".image3").toggle(1200);
-        $(".image3").show(1200);
-     });
- });
- $(document).ready(function(){
-     $('.text').hover(function () {
-     $(this).animate({opacity:'1'});
-     },
-     function () {
-         $(this).animate({opacity:'0'});
-     })
- });
- $(document).ready(function(){
-     $('#submission').submit(function(event){
-         var display1 = $('input:first').val();
-         var display2 = $('input#mail').val();
-         if(display1 && display2 != ''){
-             alert ('Thank You ' + display1 + ' your subscription is well received');
-         }else {
-             alert ('Invalid input');
-         }
-         event.preventDefault();
-     })
- });
+    $("div#content").click(function() {
+      $("div#content").toggle();
+      $("div#design").toggle();
+    });
+    $("div#dev").click(function() {
+      $("div#dev-content").toggle();
+      $("div#dev").toggle();
+    });
+    $("div#dev-content").click(function() {
+      $("div#dev-content").toggle();
+      $("div#dev").toggle();
+    });
+    $("div#product").click(function() {
+      $("div#prod-content").toggle();
+      $("div#product").toggle();
+    });
+    $("div#prod-content").click(function() {
+      $("div#prod-content").toggle();
+     $("div#product").toggle();
+    });
+  
+    $("#col-1").hover(function() {
+      $("#text1").fadeTo("slow",1);
+      $("#photo1").fadeTo("slow",0.4);
+      },function() {
+        $("#text1").fadeTo("slow",0);
+        $("#photo1").fadeTo("slow",1);
+      });
+    $("#col-2").hover(function() {
+      $("#text2").fadeTo("slow",1);
+      $("#photo2").fadeTo("slow",0.4);
+      },function() {
+        $("#text2").fadeTo("slow",0);
+        $("#photo2").fadeTo("slow",1);
+      });
+    $("#col-3").hover(function() {
+      $("#text3").fadeTo("slow",1);
+      $("#photo3").fadeTo("slow",0.4);;
+      },function() {
+        $("#text3").fadeTo("slow",0);
+        $("#photo3").fadeTo("slow",1);
+      });
+    $("#col-4").hover(function() {
+      $("#text4").fadeTo("slow",1);
+      $("#photo4").fadeTo("slow",0.4);
+      },function() {
+        $("#text4").fadeTo("slow",0);
+        $("#photo4").fadeTo("slow",1);
+      });
+    $("#col-5").hover(function() {
+      $("#text5").fadeTo("slow",1);
+      $("#photo5").fadeTo("slow",0.4);
+      },function() {
+        $("#text5").fadeTo("slow",0);
+        $("#photo5").fadeTo("slow",1);
+      });
+    $("#col-6").hover(function() {
+      $("#text6").fadeTo("slow",1);
+      $("#photo6").fadeTo("slow",0.4);
+      },function() {
+        $("#text6").fadeTo("slow",0);
+        $("#photo6").fadeTo("slow",1);
+      });
+    $("#col-7").hover(function() {
+      $("#text7").fadeTo("slow",1);
+      $("#photo7").fadeTo("slow",0.4);
+      },function() {
+        $("#text7").fadeTo("slow",0);
+        $("#photo7").fadeTo("slow",1);
+      });
+    $("#col-8").hover(function() {
+      $("#text8").fadeTo("slow",1);
+      $("#photo8").fadeTo("slow",0.4);
+      },function() {
+        $("#text8").fadeTo("slow",0);
+        $("#photo8").fadeTo("slow",1);
+      });
+      //Business Logic
+      $("form").submit(function() {
+        var name= $("input#name").val();
+        alert("We've received your message "+name+". Thank you for contacting us");
+        event.preventDefault();
+      });
+  });
